321e4d0e06b0677bc460f41d700a9ba0f4e3971d
AdarshMurali <137914842+AdarshMurali@users.noreply.github.com>
2023-07-29 23:12:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,32 @@
-from flask import Flask
+from flask import Flask,jsonify
 app = Flask(__name__)
+
+# Import required libraries
+import pyodbc
+
+# Replace these with your database connection details
+server = 'portfoliorecomendation.database.windows.net'
+database = 'Portfolio'
+username = 'PortRecom'
+password = 'Portfolio@007'
+
+# Create the database connection
+connection_string = f'DRIVER={{SQL Server}};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+connection = pyodbc.connect(connection_string)
 
 @app.route('/api/bye')
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
+
+@app.route('/api/hello')
+def hello_geek():
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM dbo.Clients')
+    data = cursor.fetchall()
+    cursor.close()
+    result = [{'id': row[0], 'name': row[1]} for row in data]
+    return jsonify(result)
+    #return '<h1>Hello from Flask & Docker</h2>'
 
 
 if __name__ == "__main__":
